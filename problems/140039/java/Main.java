6d03172356737e8e1fbf1ccaf146a23d4500fbea
Mohammad Amin <m.amin.arjang@gmail.com>
2023-04-11 01:44:25 +0330

    Create Main.java for 140039

---
+++ problems/140039/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.String;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("quera!");
+    }
+}
